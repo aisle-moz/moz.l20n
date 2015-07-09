7eece2bca4dab090601950e2250a61920c48b3ac
Axel Hecht <axel@pike.org>
2015-07-09 17:47:57 +0200

    language mode, higlight junk nodes, add outline
    Junk nodes are multiline, so we need to split them up when
    converting them to markers to be one per line.

--- language/worker.js
+++ language/worker.js
@@ -20,37 +20,83 @@ l20n.handlesLanguage = function(language) {
 l20n.parse = function(value, callback) {
     console.log('l20n worker parse');
     var ast = L20nParser.parse(null, value, true);
-    var errors = [];
     var that = this;
-    ast._errors.forEach(function(e) {
-        var spos = that.doc.indexToPosition(e._pos.start);
-        var epos = that.doc.indexToPosition(e._pos.start + 1);
-        var node = tree.cons("Error", [tree.string(e.message + ' in column ' + (spos.column + 1))]);
+    var entries = ast.body.map(function(entry) {
+        var spos = that.doc.indexToPosition(entry._pos.start);
+        var epos = that.doc.indexToPosition(entry._pos.end);
+        var children = [];
+        if (entry.type === 'Entity') {
+            children.push(tree.string(entry.id.name));
+        }
+        var node = tree.cons(entry.type, children);
         node.setAnnotation('pos', {
             sc: spos.column,
             sl: spos.row,
             ec: epos.column,
             el: epos.row
         });
-        errors.push(node);
+        return node;
     });
-    callback(tree.cons("Errors", errors));
+    var errors = ast._errors.map(function(e) {
+        var spos = that.doc.indexToPosition(e._pos.start);
+        var epos = that.doc.indexToPosition(e._pos.start + 1);
+        var node = tree.cons("Error", [
+            tree.string(e.message + ' in column ' + (spos.column + 1))
+        ]);
+        node.setAnnotation('pos', {
+            sc: spos.column,
+            sl: spos.row,
+            ec: epos.column,
+            el: epos.row
+        });
+        return node;
+    });
+    callback(tree.cons("AST", [
+        tree.cons("Body", entries),
+        tree.cons("Errors", errors)
+    ]));
 };
 
 l20n.outline = function(doc, fullAst, callback) {
-    var items = [];
+    var items = fullAst.collectTopDown('Entity(id)', function(match) {
+            return {
+                icon: 'method',
+                name: match.id.value,
+                pos: {sl: this.getPos().sl}
+            };
+        }).toArray();
     callback({items: items});
 };
 
 l20n.analyze = function(value, fullAst, callback, minimalAnalysis) {
     console.log('l20n worker analyze');
-    var items = fullAst.map(function(e) {
+    var that = this;
+    var items = fullAst[1].map(function(e) {
         return {
             pos: e.getPos(),
             message:e[0].value,
             type: "error"
         };
     }).toArray();
+    fullAst.collectTopDown('JunkEntry()')
+        .each(function(junk) {
+            // split junk entries if they span lines, c9 can't deal
+            var pos = junk.getPos();
+            for (var line = pos.sl; line <= pos.el; line++) {
+                var marker = {
+                    type: 'error',
+                    pos: {
+                        sl: line,
+                        el: line,
+                        sc: line === pos.sl ? pos.sc : 0,
+                        ec: line === pos.el ? pos.ec : that.doc.getLine(line).length
+                    }
+                };
+                if (marker.pos.sc !== marker.pos.ec) {
+                    items.push(marker);
+                }
+            }
+        });
     callback(items);
 };
 
